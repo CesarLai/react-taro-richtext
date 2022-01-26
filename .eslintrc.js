bc00f22265c8f4e556a5a5f485179d47c25b230d
Cesar Lai <git@cesarlai.com>
2022-01-26 15:27:49 +0800

    feat: 修改配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   ignorePatterns: ['*.js'],
   rules: {
     'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'jsx-quotes': ['error', 'prefer-double']
   }
 }
